a79281d67007a1963066bba13a4bd3e444255140
leylakhalilli <leylaxelilli65@gmail.com>
2022-10-04 17:44:29 +0400

    polymorphismDemo

---
+++ src/polymorphismDemo/CustomerManager.java
@@ -0,0 +1,15 @@
+package polymorphismDemo;
+
+public class CustomerManager {
+    private BaseLogger baseLogger;
+
+    public CustomerManager(BaseLogger baseLogger) {
+        this.baseLogger = baseLogger;
+    }
+
+    public void add() {
+        System.out.println("added");
+        this.baseLogger.log("log message-");
+
+    }
+}
---
+++ src/polymorphismDemo/Main.java
@@ -0,0 +1,17 @@
+package polymorphismDemo;
+
+public class Main {
+    public static void main(String[] args) {
+//        EmailLogger emailLogger = new EmailLogger();
+//        emailLogger.log("email log message");
+
+//        BaseLogger[] loggers = new BaseLogger[]{new FileLogger(), new EmailLogger(), new DatabaseLogger()};
+//        for (BaseLogger logger:loggers){
+//            logger.log("log mesaj");
+//        }
+
+        CustomerManager customerManager=new CustomerManager(new DatabaseLogger());
+        customerManager.add();
+    }
+
+}
